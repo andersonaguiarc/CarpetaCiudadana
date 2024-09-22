fee5eaf6709f7b7245af42833c9ffa7c2587d66f
Anderson Aguiar <andersonaguiar@MacBook-Air-de-Anderson.local>
2024-09-22 12:56:33 -0500

    Versión inicial servicio CitizensTransfers

--- services/citizen_transfer/src/main/citizen_transfers.py
+++ services/citizen_transfer/src/main/citizen_transfers.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+import requests
 from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
@@ -19,9 +20,26 @@ class CitizensTransfer(Resource):
             operator_id = data['operator_id']
 
             # Lógica de transferencia del ciudadano
-            # ...
+            # Revisar JWT
+            jwt_token = "jwt generado"
 
-            return {"message": f"El ciudadano {citizen_id} ha sido transferido al {operator_id} de forma exitosa."}, 200
+            # Consumir el API de Citizen para hacer unregister del ciudadano
+            citizen_api_url = "http://134.122.25.105:30000/users/api/citizens/transfer"
+            headers = {
+                "Authorization": f"Bearer {jwt_token}"
+            }
+
+            # Realizar la petición PATCH al servicio Citizen
+            response = requests.patch(citizen_api_url, headers=headers)
+
+            # Verificar la respuesta del servicio de Citizen para continuar
+            if response.status_code == 200:
+                return {"message": f"El ciudadano {citizen_id} ha sido transferido al {operator_id} de forma exitosa."}, 200
+            else:
+                # Lógica de resiliencia
+                # ...
+                return {"message": f"Transferencia del ciudadano {citizen_id} en proceso."}, 200
+                #return {"message": "Falló la transferencia del ciudadano en el servicio Citizen.", "details": response.text}, 500
 
         except BadRequest as e:
             return {"message": "Formato JSON inválido."}, 400
@@ -29,7 +47,7 @@ class CitizensTransfer(Resource):
             return {"message": str(e)}, 500
 
 # Ruta y recurso para el API
-api.add_resource(CitizensTransfer, '/citizen/transfer')
+api.add_resource(CitizensTransfer, '/transfers/api/citizens/transfer')
 
 if __name__ == '__main__':
     app.run(debug=True)
